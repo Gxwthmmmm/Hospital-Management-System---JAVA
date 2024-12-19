31f81dc3085972f524c0add52656e824e5f22395
Gxwthmmmm <gowthammahendrada@gmail.com>
2024-12-19 20:08:26 +0530

    Update Hospitalmanagement.java

--- Hospitalmanagement.java
+++ Hospitalmanagement.java
@@ -1,15 +1,6 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.util.ArrayList;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
 class Person{
     public String name;
     public int age;
